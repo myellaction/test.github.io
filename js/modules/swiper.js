92d97551e9b38b009d79132013477fc3a0a1a514
myellaction <dimaplkharkiv@gmail.com>
2024-04-19 20:14:30 +0300

    touch9

--- js/modules/swiper.js
+++ js/modules/swiper.js
@@ -1,10 +1,12 @@
+const maxWidth = window.innerWidth;
 const swiper = new Swiper(".tech__swiper", {
     slidesPerView: 4,
     loop: true,
-    spaceBetween: 100,
+    spaceBetween: maxWidth > 768 ? 100 : 70,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
     speed: 1000,
 });
+
