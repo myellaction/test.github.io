ebcf634865292fe82988e554cd4e764f390ebb8c
myellaction <dimaplkharkiv@gmail.com>
2024-04-23 12:26:31 +0300

    cross

--- js/compiled/compiled_script.js
+++ js/compiled/compiled_script.js
@@ -287,7 +287,7 @@ const maxWidth = window.innerWidth;
 const swiper = new Swiper(".tech__swiper", {
   slidesPerView: maxWidth >= 767 ? 4 : 3,
   loop: true,
-  spaceBetween: maxWidth >= 991 ? 100 : 70,
+  spaceBetween: maxWidth >= 991 ? 100 : maxWidth >= 575 ? 70 : 20,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false
